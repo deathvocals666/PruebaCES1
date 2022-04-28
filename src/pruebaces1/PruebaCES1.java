54861d2819d8bf56d71293634e9f83119a91787f
deathvocals666@gmail.com <>
2022-04-28 18:42:31 -0500

    Prueba Ces1

---
+++ src/pruebaces1/PruebaCES1.java
@@ -0,0 +1,13 @@
+package pruebaces1;
+
+public class PruebaCES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Prueba Github Brito 28/04/2022");
+    }
+    
+}
